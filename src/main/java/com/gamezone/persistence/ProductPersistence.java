cf9a48d620b570301c6a4fb0ebe01e2e85243a21
LUIS EDUARDO GOMEZ ROSALES <luiseduardogomez@unicesar.edu.co>
2026-09-07 16:51:21 -0500

    feat: add ProductPersistence for file-based storage according to class diagram

---
+++ src/main/java/com/gamezone/persistence/ProductPersistence.java
@@ -0,0 +1,47 @@
+package com.gamezone.persistence;
+
+import com.gamezone.model.Product;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Persistence class for Product objects.
+ * Handles saving and loading products to/from a file.
+ */
+public class ProductPersistence {
+
+    private static final String FILE_NAME = "products.dat";
+
+    /**
+     * Saves a list of products to a file.
+     * 
+     * @param products The list of products to save
+     */
+    public void save(List<Product> products) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
+            oos.writeObject(products);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Loads all products from the file.
+     * 
+     * @return A list of products loaded from the file
+     */
+    public List<Product> loadAll() {
+        File file = new File(FILE_NAME);
+        if (!file.exists()) {
+            return new ArrayList<>();
+        }
+        
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+            return (List<Product>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+}
