5fdff5f526a5da13798afe5748445ede5b453792
Esteban raul vergara jaime <ervergara@unicesar.edu.co>
2026-09-23 20:45:31 -0500

    feat: wire accessory module in Main

--- src/main/java/com/gamezone/Main.java
+++ src/main/java/com/gamezone/Main.java
@@ -1,8 +1,10 @@
 package com.gamezone;
 
+import com.gamezone.persistence.AccessoryPersistence;
 import com.gamezone.persistence.PersonPersistence;
 import com.gamezone.persistence.ProductPersistence;
 import com.gamezone.persistence.SalePersistence;
+import com.gamezone.service.AccessoryService;
 import com.gamezone.service.PersonService;
 import com.gamezone.service.ProductService;
 import com.gamezone.service.SaleService;
@@ -16,12 +18,15 @@ public class Main {
 
         ProductService productService = new ProductService();
 
+        AccessoryPersistence accessoryPersistence = new AccessoryPersistence();
+        AccessoryService accessoryService = new AccessoryService(accessoryPersistence);
+
         SalePersistence salePersistence = new SalePersistence();
-        SaleService saleService = new SaleService(salePersistence, productService);
+        SaleService saleService = new SaleService(salePersistence, productService, accessoryService);
 
         personService.preloadVendorsIfEmpty();
 
-        ConsoleUI ui = new ConsoleUI(personService, productService, saleService);
+        ConsoleUI ui = new ConsoleUI(personService, productService, saleService, accessoryService);
         ui.showMainMenu();
     }
 }
